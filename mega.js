013a94273fce7c9fd0cf1d89d5455c760922e7de
SILENTLOVER432 <newheroku00@gmail.com>
2024-11-03 12:55:23 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,6 +1,6 @@
 const mega = require("megajs");
 const auth = {
-    email: 'princemds432@yahoo.com',
+    email: 'sobxsparkle085@gmail.com',
     password: '002821002821#Silen',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
